4cb93bc3ae0b4f89a3897dee96a86abda845e8f0
santos <gabsanto94@gmail.com>
2018-07-05 13:54:50 -0400

    - added user service for registration

---
+++ src/main/java/com/revature/bikeshop/services/UserService.java
@@ -0,0 +1,17 @@
+package com.revature.bikeshop.services;
+
+import com.revature.bikeshop.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    void addUser (User user);
+
+    User getUsersById (int userId);
+
+    List<User> getAllUsers();
+
+    User getUserByUsername (String username);
+
+}
