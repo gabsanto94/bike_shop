8d306dc2d6a46adfef9d85abf3ae607b06a1a2fe
santos <gabsanto94@gmail.com>
2018-07-05 13:54:13 -0400

    - added Register Controller

--- src/main/java/com/revature/bikeshop/controller/RegisterController.java
+++ src/main/java/com/revature/bikeshop/controller/RegisterController.java
@@ -1,18 +1,15 @@
 package com.revature.bikeshop.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.bikeshop.model.BillingAddress;
+import com.revature.bikeshop.model.ShippingAddress;
 import com.revature.bikeshop.model.User;
 import com.revature.bikeshop.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.PrintWriter;
-
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class RegisterController {
@@ -23,19 +20,33 @@ public class RegisterController {
     private UserService userService;
 
     @RequestMapping(value="/register", method=RequestMethod.GET)
-    @ResponseBody
-    public String getUser() throws JsonProcessingException {
+    public @ResponseBody String registerUser(HttpSession session) {
+
+        //initialize objects
+        User user = new User();
+        BillingAddress ba = new BillingAddress();
+        ShippingAddress sa = new ShippingAddress();
+
+        user.setBillingAddress(ba);
+        user.setShippingAddress(sa);
+
+        session.setAttribute("user", user);
+
+        System.out.println("I am initializing stuff" + user);
 
         return "hello from the register controller";
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
-    private @ResponseBody String registerUser(@RequestBody User u) {
+    public @ResponseBody String registerUser(@RequestBody User u) {
 
-        System.out.println(u);
-        //userService.addUser(u);
+        System.out.println("im response" + u);
 
-        return "redirect:home";
+        userService.addUser(u);
+
+        System.out.println("after insert" + u);
+
+        return "redirect: home";
     }
 
 }
