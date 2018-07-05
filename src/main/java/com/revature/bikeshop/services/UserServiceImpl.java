ca0536e514b562d366addfb0b4b94d44ec4e8c23
santos <gabsanto94@gmail.com>
2018-07-05 14:18:14 -0400

    - add implementation of user service

---
+++ src/main/java/com/revature/bikeshop/services/UserServiceImpl.java
@@ -0,0 +1,36 @@
+package com.revature.bikeshop.services;
+
+import com.revature.bikeshop.dao.UserDAO;
+import com.revature.bikeshop.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    @Autowired
+    private UserDAO userDao;
+
+    @Override
+    public void addUser(User user) {
+        System.out.println("im inside the service" + user);
+        userDao.addUser(user);
+    }
+
+    @Override
+    public User getUsersById(int userId) {
+        return userDao.getUserById(userId);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
+
+    @Override
+    public User getUserByUsername(String username) {
+        return userDao.getUserbyUsername(username);
+    }
+}
