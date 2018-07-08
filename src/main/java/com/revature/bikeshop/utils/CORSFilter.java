92bec97d02aeeb9c8b0acbe002762bd75d9fdd23
santos <gabsanto94@gmail.com>
2018-07-08 00:30:31 -0400

    -added Filter to allow Cross Origin

---
+++ src/main/java/com/revature/bikeshop/utils/CORSFilter.java
@@ -0,0 +1,30 @@
+package com.revature.bikeshop.utils;
+
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CORSFilter extends GenericFilterBean implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+        httpResponse.setHeader("Access-Control-Allow-Origin", "*");
+        //httpResponse.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+        //* to allow all members
+        httpResponse.setHeader("Access-Control-Allow-Methods", "*");
+
+        //Second Parameter: it can be Origin, Content-Type, X-Auth-Token, Authorization
+        httpResponse.setHeader("Access-Control-Allow-Headers", "*");
+
+        httpResponse.setHeader("Access-Control-Allow-Credentials", "false");
+        httpResponse.setHeader("Access-Control-Max-Age", "3600");
+
+        filterChain.doFilter(servletRequest, servletResponse);
+
+        System.out.println("CROSS ORIGIN FINISH SETTING UP....");
+    }
+}
