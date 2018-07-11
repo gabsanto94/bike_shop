6c3ec1e07e1878da55d8aa4b24fcf3a0b11cb893
Theodis Thompson <theothomp37@gmail.com>
2018-07-10 23:56:11 -0400

    Cart Controller

---
+++ src/main/java/com/revature/bikeshop/controller/CartController.java
@@ -0,0 +1,64 @@
+package com.revature.bikeshop.controller;
+
+
+
+
+import com.revature.bikeshop.dao.CartItemDAO;
+import com.revature.bikeshop.model.Cart;
+import com.revature.bikeshop.model.CartItem;
+import com.revature.bikeshop.services.CartItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+public class CartController {
+
+
+    @Autowired
+    private CartItemService cartItemService;
+
+    @Autowired
+    private CartItemDAO cartItemDAO;
+
+
+    //delete - call DAO to remove items from the database
+    @RequestMapping(method = RequestMethod.DELETE)
+    public  @ResponseBody void deleteCart(Cart cart){
+        cartItemDAO.removeAllCartItems(cart);
+    }
+
+    //@PathVariable int id
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+   private @ResponseBody void removeItems(@PathVariable int id){
+        cartItemService.removeCartItem(id);
+    }
+
+
+    @RequestMapping(method = RequestMethod.GET)
+    private List<CartItem> getCartItems(){
+
+        return cartItemService.getAllItems();
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public @ResponseBody String addItems(@RequestBody CartItem ci){
+        System.out.println("im here");
+        cartItemService.addCartItem(ci);
+        return "Added Successfully.";
+    }
+
+
+
+    //delete item from cart
+    // /{id}
+    //public @ResponseBody String deleteItem(@RequestBody CartItem ci){
+
+       // cartItemService.removeCartItem(ci);
+      //  return "item removed.";
+
+}
+
--- src/main/java/com/revature/bikeshop/utils/FireHibernate.java
+++ src/main/java/com/revature/bikeshop/utils/FireHibernate.java
@@ -2,9 +2,7 @@ package com.revature.bikeshop.utils;
 
 import com.revature.bikeshop.dao.UserDAO;
 import com.revature.bikeshop.dao.UserDAOImp;
-import com.revature.bikeshop.model.BillingAddress;
-import com.revature.bikeshop.model.ShippingAddress;
-import com.revature.bikeshop.model.User;
+import com.revature.bikeshop.model.*;
 import org.hibernate.Session;
 
 public class FireHibernate {
@@ -20,13 +18,16 @@ public class FireHibernate {
         org.hibernate.Transaction tx = session.beginTransaction();
         BillingAddress ba = new BillingAddress(0, "325 East Mc Dowell", "Mesa", "Arizona", "85203", null);
         ShippingAddress sa = new ShippingAddress(0, "454 East Dowell", "Mesa", "Arizona", "85203", null);
-        //Cart myCart = new Cart(0, null, 0, null);
         User u = new User("gabriel", "santos", "gsantos@whoa.com",
                 "gabriel", "santos", ba, sa,  "admin");
+
+        Product prd = new Product(2, "Dyson", "Dyson", "bike", "fast", 129.89, "new", 23);
+
+        Cart myCart = new Cart(1, null, 0, u);
+        CartItem ci = new CartItem(12, prd, 1,129.89, myCart );
         //myCart.setUser(u);
         ba.setUser(u);
         sa.setUser(u);
-
         userDao.addUser(u);
         tx.commit();
         System.out.println(u);
