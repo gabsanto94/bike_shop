0a5cb64ad5bdc0e02b49360088ae114bc7f2fd58
santos <gabsanto94@gmail.com>
2018-07-12 17:36:55 -0400

    -fixed authentication -returns user's authentication on response.

--- src/main/java/com/revature/bikeshop/utils/CORSFilter.java
+++ src/main/java/com/revature/bikeshop/utils/CORSFilter.java
@@ -3,6 +3,7 @@ package com.revature.bikeshop.utils;
 import org.springframework.web.filter.GenericFilterBean;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -12,18 +13,22 @@ public class CORSFilter extends GenericFilterBean implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
 
         HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+
         httpResponse.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
         //* to allow all members
-        httpResponse.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+        httpResponse.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
 
         //Second Parameter: it can be Origin, Content-Type, X-Auth-Token, Authorization
-        httpResponse.setHeader("Access-Control-Allow-Headers", "*");
+        httpResponse.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, Content-Type, Authorization");
 
         httpResponse.setHeader("Access-Control-Allow-Credentials", "false");
         httpResponse.setHeader("Access-Control-Max-Age", "3600");
 
-        filterChain.doFilter(servletRequest, servletResponse);
-
-        System.out.println("CROSS ORIGIN FINISH SETTING UP....");
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            httpResponse.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            filterChain.doFilter(request, httpResponse);
+        }
     }
 }
